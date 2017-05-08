ffad08877ea490e2231b81730d33470cd63bdeac
ValentinaRod <v.rodriguez1425@gmail.com>
2017-05-08 19:33:27 -0300

    añadiendo archivos modificado

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+var x = document.getElementById("myText").value;
+    document.getElementById("demo").innerHTML = x;
+
+function imc(peso, estatura){
+	var peso = prompt('ingrese su peso');
+	var estatura = prompt('ingrese su estatura');
+
+	alert(peso/Math.pow(altura, 2);
+}
+imc();
